7fb783e420f57fda3c6627c75005a4352671eca3
sharifmiazi <62771733+sharifmiazi@users.noreply.github.com>
2020-09-03 00:36:40 -0400

    Add project files.

---
+++ KInsurance_Repository/Developer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KInsurance_Repository
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public bool PluralSightAccount { get; set; }
+        public string Title { get; set; }
+
+        public Developer(int id, string lastName, bool pluralSightAccount, string title )
+        {
+            Id = id;
+            LastName = lastName;
+            PluralSightAccount = pluralSightAccount;
+            Title = title;
+        }
+
+
+    }
+}
---
+++ KInsurance_Repository/Team.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KInsurance_Repository
+{
+    public class Team
+    {
+        public string TeamName { get; set; }
+        public int ID { get; set; }
+
+
+
+         private List<Developer> _listOfTeam = new List<Developer>();
+            //Create
+            public void AddnewTeam(Developer Team)
+             {
+                _listOfTeam.Add(Team);
+
+             }
+            //Read
+            public List<Developer> GetNewDeveloper()
+            {
+            return _listOfTeam;
+            }
+            //Update
+            public bool UpdateExcistingDevelopers(string originalTitle, int id, string lastName, bool pluralSightAccount, string title)
+             {
+            // Find the Team
+            Developer oldTeam = getDeveloperByTitle(originalTitle);
+
+
+
+            // Update the Team
+            if (oldTeam != null)
+            {
+                oldTeam.Title = title;
+                oldTeam.Id = id;
+                oldTeam.LastName = lastName;
+                oldTeam.PluralSightAccount = pluralSightAccount;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+             }
+            //Delete
+            public bool RemoveDeveloperFromList(string title)
+            {
+            Developer Team = getDeveloperByTitle(title);
+
+                if (Team == null)
+                {
+                return false;
+                }
+
+            int initialCount = _listOfTeam.Count;
+            _listOfTeam.Remove(Team);
+
+            if (initialCount > _listOfTeam.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+            //Helper Method
+            private Developer getDeveloperByTitle(string title)
+            {
+            foreach (Developer Team in _listOfTeam)
+            {
+                if (Team.Title == title)
+                {
+                    return Team;
+                }
+                
+            }
+              return null;  
+            }
+
+    }
+}
